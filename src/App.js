ba5b708082df3d94d6c6bb9fa2a311e09fa752ef
almasrafaqat <almasrafaqat6@gmail.com>
2023-03-02 20:13:35 +0300

    Navbar Layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import HomePage from "./pages/home/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalStyle, theme } from "./GlobalStyle";
 import About from "./components/about/about.component";
+import Header from "./components/header/header.component";
+import Announcement from "./components/announcement/announcement.component";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </ThemeProvider>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,13 +14,10 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   height: 100%;
-  padding: ${(props) => (props.type !== "menu" ? "30px 20px" : "0px 20px")};
+  padding: 30px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border-top: ${(props) => (props.type === "menu" ? "1px solid #ececec" : "")};
-  border-bottom: ${(props) =>
-    props.type === "menu" ? "1px solid #ececec" : ""};
 `;
 
 const Left = styled.div`
@@ -103,31 +100,103 @@ const UserContainer = styled.div`
 
 const UserIcon = styled(Person2Outlined)``;
 
+/** Navbar Design */
+
+/**Mega Menu */
+
+const Nav = styled.div`
+  padding: 0px 20px;
+  border-top: 1px solid #ececec;
+  border-bottom: 1px solid #ececec;
+  position: fixed;
+  width: 100%;
+  z-index: 99;
+`;
+
+const NavWrapper = styled.div`
+  max-width: 1300px;
+  height: 70px;
+  line-height: 70px;
+  position: relative;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const MenuItem = styled.ul`
+  display: inline-flex;
+`;
+
+const DropMenu = styled.ul`
+  position: absolute;
+  background: #242526;
+  width: 180px;
+  top: 85px;
+  line-height: 45px;
+  border-radius: 5px;
+  opacity: 0;
+  visibility: visible;
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15);
+`;
+
+const ItemList = styled.li`
   list-style: none;
-  display: flex;
+
+  &:hover ${DropMenu} {
+    transition: all 0.3s ease;
+    top: 70px;
+    opacity: 1;
+    visibility: visible;
+  }
 `;
 
-const ListItem = styled.li`
-  margin-right: 50px;
-  display: flex;
-  align-items: center;
-`;
-
-const Deals = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.border};
-  color: ${({ theme }) => theme.colors.white};
-  border-radius: 8px;
-  padding: 12px;
-`;
-
-const DealIcon = styled.span``;
-const DealText = styled.span`
+const ItemLink = styled(Link)`
+  padding: 9px 15px;
+  border-radius: 5px;
   font-size: 18px;
-  font-weight: 300;
-  color: ${({ theme }) => theme.colors.white};
+  font-weight: 500;
+  color: #f2f2f2;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: #3a3b3c;
+  }
+`;
+
+/**Mega Menu */
+
+const MegaBox = styled.div`
+  background: gray;
+  position: absolute;
+  left: 0;
+  width: 100%;
+  padding: 0px 30px;
+  top: 85px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15);
+  padding: 30px 20px;
+  width: 100%;
+`;
+
+const HeaderTitle = styled.header`
+  color: #f2f2f2;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const Row = styled.div`
+  width: calc(25% - 30px);
+  line-height: 45px;
+`;
+
+const MegaItem = styled.ul`
+  margin-left: -40px;
+  border-left: 1px solid rgba(255, 255, 255, 0.09);
 `;
 
 const Header = () => {
@@ -166,32 +235,84 @@ const Header = () => {
           </UserContainer>
         </Right>
       </Wrapper>
-      <Wrapper type="menu">
-        <MenuItem>
-          <ListItem type="deals">
-            <Deals>
-              <DealIcon>
-                <LocalFireDepartmentOutlined />
-              </DealIcon>
-              <Link to="/">
-                <DealText>Deals</DealText>
-              </Link>
-            </Deals>
-          </ListItem>
-          <ListItem>
-            <Link to="/">Home</Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/about">About</Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/about">Contact</Link>
-          </ListItem>
-          <ListItem>
-            <Link to="/about">Mega Menu</Link>
-          </ListItem>
-        </MenuItem>
-      </Wrapper>
+      <Nav>
+        <NavWrapper>
+          <MenuItem>
+            <ItemList>
+              <ItemLink to="/">Home</ItemLink>
+            </ItemList>
+            <ItemList>
+              <ItemLink to="/">About</ItemLink>
+            </ItemList>
+            <ItemList>
+              <ItemLink to="/">DropDown</ItemLink>
+              <DropMenu type="dropdown">
+                <ItemList>
+                  <ItemLink>DropDown 1</ItemLink>
+                </ItemList>
+                <ItemList>
+                  <ItemLink>DropDown 1</ItemLink>
+                </ItemList>
+                <ItemList>
+                  <ItemLink>DropDown 1</ItemLink>
+                </ItemList>
+                <ItemList>
+                  <ItemLink>DropDown 1</ItemLink>
+                </ItemList>
+              </DropMenu>
+            </ItemList>
+            <ItemList>
+              <ItemLink>Mega Menu</ItemLink>
+              <MegaBox>
+                <Content>
+                  <Row>
+                    <HeaderTitle>Programming</HeaderTitle>
+                    <MegaItem>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                    </MegaItem>
+                  </Row>
+                  <Row>
+                    <HeaderTitle>Coding</HeaderTitle>
+                    <MegaItem>
+                      <ItemList>
+                        <ItemLink>Coding 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Coding 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Coding 1</ItemLink>
+                      </ItemList>
+                    </MegaItem>
+                  </Row>
+                  <Row>
+                    <HeaderTitle>Programming</HeaderTitle>
+                    <MegaItem>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                      <ItemList>
+                        <ItemLink>Service 1</ItemLink>
+                      </ItemList>
+                    </MegaItem>
+                  </Row>
+                </Content>
+              </MegaBox>
+            </ItemList>
+          </MenuItem>
+        </NavWrapper>
+      </Nav>
     </Container>
   );
 };
