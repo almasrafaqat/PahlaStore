a41ae6f33892828935c0ab4b129b5c62d1912db9
Almas <almasrafaqat6@gmail.com>
2023-03-05 11:20:45 +0300

    Mega Menu Layout

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -56,22 +56,84 @@ const DropDownItem = styled(MenuItem)`
 
 /**Mega Menu */
 
-const MegaBox = styled.div``;
+const MegaBox = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  padding: 0px 30px;
+  top: 85px;
+  opacity: 0;
+  visibility: hidden;
+`;
 
-const Content = styled.div``;
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: #ececec;
+  padding: 25px 20px;
+  width: 100%;
+  box-shadow: 0 6px 10px rgba(0,0,0,0.15);
+  border-radius: 8;
+`;
 
-const Row = styled.div``;
+const Row = styled.div`
+  width: calc(25% - 30px);
+  line-height: 45px;
+`;
 
-const HeaderTitle = styled.header``;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
+const HeaderTitle = styled.header`
+color: teal;
+font-size: 20px;
+font-weight: 500;
+`;
 
 const MegaItem = styled(MenuItem)`
   display: block;
+  
+ 
+`;
+
+const MegaItemChild = styled(MegaItem)`
+  margin-left: -40px;
+  border-left: 1px solid teal;
+
+  &:nth-child(1){
+    border-left: 5px solid teal;
+    margin-left: 90px;
+  }
+`;
+
+
+
+const MegaLinks = styled(Link)`
+   padding: 0px;
+  padding: 0 20px;
+  font-size: 17px;
+  display: block;
+
+  &:hover {
+    background-color: teal;
+    color: #fff;
+  }
 `;
 
 const MenuList = styled.li`
   list-style: none;
+  padding: ${props => props.type === 'mega-list' ? "0 20px" : ""};
 
   &:hover ${DropDownItem} {
+    transition: all 0.4s ease;
+    top: 70px;
+    opacity: 1;
+    visibility: visible;
+  }
+  &:hover ${MegaBox} {
     transition: all 0.4s ease;
     top: 70px;
     opacity: 1;
@@ -116,11 +178,59 @@ const Nav = () => {
               <MegaBox>
                 <Content>
                   <Row>
-                    <HeaderTitle>Graphic Service</HeaderTitle>
-                    <MenuList>
-                      <MenuLink>Graphic Service 1</MenuLink>
-                    </MenuList>
+                    <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVyoBVQg0JEHNq-rbOPU1WWxj9U3ArpmMlhulg6CjUf3XkRTSUCZqNWNjIYJZ58u56JGs&usqp=CAU" />
                   </Row>
+                  <Row>
+                    <HeaderTitle>Graphic Service</HeaderTitle>
+                    <MegaItemChild>
+                      <MenuList type="mega-list">
+                        <MegaLinks>Graphic Service 1 </MegaLinks>
+                      </MenuList>
+                      <MenuList type="mega-list">
+                        <MegaLinks>Graphic Service 2 </MegaLinks>
+                      </MenuList>
+                      <MenuList type="mega-list">
+                        <MegaLinks>Graphic Service 3 </MegaLinks>
+                      </MenuList>
+                      <MenuList type="mega-list">
+                        <MegaLinks>Graphic Service 4 </MegaLinks>
+                      </MenuList>
+                    </MegaItemChild>
+                  </Row>
+                  <Row>
+                  <HeaderTitle>Graphic Service</HeaderTitle>
+                  <MegaItemChild>
+                    <MenuList type="mega-list">
+                      <MegaLinks>Graphic Service 1 </MegaLinks>
+                    </MenuList>
+                    <MenuList type="mega-list">
+                      <MegaLinks>Graphic Service 2 </MegaLinks>
+                    </MenuList>
+                    <MenuList type="mega-list">
+                      <MegaLinks>Graphic Service 3 </MegaLinks>
+                    </MenuList>
+                    <MenuList type="mega-list">
+                      <MegaLinks>Graphic Service 4 </MegaLinks>
+                    </MenuList>
+                  </MegaItemChild>
+                </Row>
+                <Row>
+                <HeaderTitle>Graphic Service</HeaderTitle>
+                <MegaItemChild>
+                  <MenuList type="mega-list">
+                    <MegaLinks>Graphic Service 1 </MegaLinks>
+                  </MenuList>
+                  <MenuList type="mega-list">
+                    <MegaLinks>Graphic Service 2 </MegaLinks>
+                  </MenuList>
+                  <MenuList type="mega-list">
+                    <MegaLinks>Graphic Service 3 </MegaLinks>
+                  </MenuList>
+                  <MenuList type="mega-list">
+                    <MegaLinks>Graphic Service 4 </MegaLinks>
+                  </MenuList>
+                </MegaItemChild>
+              </Row>
                 </Content>
               </MegaBox>
             </MegaItem>
