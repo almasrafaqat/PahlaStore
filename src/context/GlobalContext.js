2b5b3f70556f14a6764f4b56c253518a81d537ae
almasrafaqat <almasrafaqat6@gmail.com>
2023-03-13 21:50:37 +0300

    Slider Designed

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -8,12 +8,26 @@ export const GlobalContextProvider = ({ children }) => {
     dropdown: false,
     megaMenu: false,
   });
+  /** For Slider */
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const SlideHandler = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+    console.log("slideIndex:", slideIndex);
+  };
 
   const value = {
     isMenuOpen,
     setIsMenuOpen,
     isDropdownOpen,
     setIsDropdownOpen,
+    slideIndex,
+    SlideHandler,
   };
 
   return (
