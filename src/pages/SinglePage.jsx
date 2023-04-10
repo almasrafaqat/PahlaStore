abf314758f33e74665b8289a2455155215dccb16
almasrafaqat <almasrafaqat6@gmail.com>
2023-04-11 02:32:34 +0300

    Product Details Page (cart Promo)

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,49 +1,43 @@
-import styled from 'styled-components';
-import '../responsive.css';
-import Header from '../components/header/header.component';
-import HeaderMobile from '../components/header-mobile/header-mobile.component';
-import Announcement from '../components/announcement/announcement.component';
-import BasicBreadcrumbs from '../components/breadcrumb/breadcrumb.component';
-import { PaddingContainer } from '../GlobalStyle';
-import ProductDetails from '../components/product-details/product-details.component';
-import NewsLetter from '../components/newsletter/newsletter.component';
-import Footer from '../components/footer/footer.component';
-import ProductBought from '../components/product-bought/product-bought.component';
-import ProductRelated from '../components/product-related/product-related.component';
+import styled from "styled-components";
+import "../responsive.css";
+import Header from "../components/header/header.component";
+import HeaderMobile from "../components/header-mobile/header-mobile.component";
+import Announcement from "../components/announcement/announcement.component";
+import BasicBreadcrumbs from "../components/breadcrumb/breadcrumb.component";
+import { PaddingContainer } from "../GlobalStyle";
+import ProductDetails from "../components/product-details/product-details.component";
+import NewsLetter from "../components/newsletter/newsletter.component";
+import Footer from "../components/footer/footer.component";
+import ProductBought from "../components/product-bought/product-bought.component";
+import ProductRelated from "../components/product-related/product-related.component";
 
-const Container = styled.div`
-    
-`;
-const Wrapper = styled.div`
-
-`;
+const Container = styled.div``;
+const Wrapper = styled.div``;
 
 const Row = styled(PaddingContainer)`
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 `;
 
 const SinglePage = () => {
-    return (
-        <Container>
+  return (
+    <Container>
+      <Announcement />
+      <Wrapper className="Desktop">
+        <Header />
+      </Wrapper>
+      <Wrapper className="Mobile">
+        <HeaderMobile />
+      </Wrapper>
+      <Row>
+        <BasicBreadcrumbs />
+      </Row>
+      <ProductDetails />
+      <ProductBought />
+      <ProductRelated />
+      <NewsLetter />
+      <Footer />
+    </Container>
+  );
+};
 
-            <Announcement />
-            <Wrapper className='Desktop'>
-                <Header />
-            </Wrapper>
-            <Wrapper className='Mobile'>
-                <HeaderMobile />
-            </Wrapper>
-            <Row>
-                <BasicBreadcrumbs />
-            </Row>
-            <ProductDetails />
-            <ProductBought />
-            <ProductRelated />
-            <NewsLetter />
-            <Footer />
-
-        </Container>
-    )
-}
-
-export default SinglePage
+export default SinglePage;
