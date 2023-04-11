d65a5efb8df4c9b140e926b540c634d8bc3a4433
almasrafaqat <almasrafaqat6@gmail.com>
2023-04-12 02:38:30 +0300

    date fixing

--- src/helpers/DeliveryDate.js
+++ src/helpers/DeliveryDate.js
@@ -2,6 +2,7 @@
 
 const DeliveryDate = (day) => {
     let date = new Date();
+    date.setDate(10);
     var months = [
         'january',
         'February',
@@ -24,12 +25,29 @@ const DeliveryDate = (day) => {
         'Thursday',
         'Friday',
         'Saturday'
-      ]
+      ];
+
+    //   var options = { weekday: 'long' };
+    //   var currentDate = date.toLocaleDateString('en-US')
+    // var monthName = date.toLocaleString('en-US', {month: 'long'});
+    // var dayName = date.toLocaleString('en-US', {day: 'long'});
+
+    // var a = new Date();
+    //     var weekdays = new Array(7);
+    //     weekdays[0] = "Sunday";
+    //     weekdays[1] = "Monday";
+    //     weekdays[2] = "Tuesday";
+    //     weekdays[3] = "Wednesday";
+    //     weekdays[4] = "Thursday";
+    //     weekdays[5] = "Friday";
+    //     weekdays[6] = "Saturday";
+    //     var r = (weekdays[a.getDay()]);
+
 
     var monthName = months[date.getMonth()];
-    var dayName = ( days[date.getDay()] + 5);
+    var dayName = ( days[date.getDay()]);
     var dayNumber = (date.getDate() + 5);
-  return <span>{dayName}, {monthName} {dayNumber}</span>;
+  return <span>{dayName}, {monthName} {dayNumber} | day is {}</span>;
 };
 
 export default DeliveryDate;
