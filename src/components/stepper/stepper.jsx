0939343981ccea3291539db438ac1fe5d152e863
almasrafaqat <almasrafaqat6@gmail.com>
2023-05-01 21:04:00 +0300

    checkout page redesign

--- src/components/stepper/stepper.jsx
+++ src/components/stepper/stepper.jsx
@@ -4,15 +4,23 @@ import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import {
   ButtonContainer,
+  CheckIcon,
+  CheckIconContainer,
   Container,
+  DelieveryAddressDate,
+  DelieveryDate,
+  DelieveryDay,
+  DeliveryAddress,
   FlexContainer,
   FlexItems,
+  OrderConfirmMessage,
   OrderNote,
   OrderNoteButtonContainer,
   OrderNoteTable,
-  OrderNoteWrapper,
-  OrderNotes,
   OrderTableContainer,
+  Row,
+  StepperContainer,
+  StepperRow,
   Wrapper,
 } from "./stepper.style";
 import { Box } from "@mui/material";
@@ -20,6 +28,7 @@ import ScrollComponent from "../scroll-component/scroll-component";
 import { Link } from "../../GlobalStyle";
 import FormatPrice from "../../helpers/FormatPrice";
 import AddToCart from "../add-to-cart/add-to-cart.component";
+import { LocalShipping } from "@mui/icons-material";
 
 const BasicStepper = ({ stepOne, stepTwo, stepThree, stepCompleted }) => {
   const steps = [stepOne, stepTwo, stepThree];
@@ -27,87 +36,115 @@ const BasicStepper = ({ stepOne, stepTwo, stepThree, stepCompleted }) => {
   ScrollComponent();
   return (
     <Container>
-      <Box sx={{ width: "100%", textTransform: "capitalize" }}>
-        <Stepper activeStep={stepCompleted} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label} </StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
-      <Wrapper>
-        <OrderNoteButtonContainer>
-          <OrderTableContainer>
-            <OrderNoteTable>
-              <OrderNote>Your Order Summary </OrderNote>
-              <FlexContainer>
-                <FlexItems heading>Date</FlexItems>
-                <FlexItems>01 May 2023</FlexItems>
-              </FlexContainer>
-              <FlexContainer>
-                <FlexItems heading>Order Id</FlexItems>
-                <FlexItems>123456789</FlexItems>
-              </FlexContainer>
-              <FlexContainer>
-                <FlexItems heading>Payment</FlexItems>
-                <FlexItems>Cash on Delivery</FlexItems>
-              </FlexContainer>
-              <FlexContainer>
-                <FlexItems heading>Delivery Charge </FlexItems>
-                <FlexItems>
-                  <FormatPrice price="500" />
-                </FlexItems>
-              </FlexContainer>
-              <FlexContainer>
-                <FlexItems heading>Total Price </FlexItems>
-                <FlexItems heading>
-                  <FormatPrice price="2500" />
-                </FlexItems>
-              </FlexContainer>
+      <Row>
+        <StepperContainer>
+          <StepperRow>
+            <Box
+              sx={{
+                width: "100%",
+                textTransform: "capitalize",
+              }}
+            >
+              <Stepper activeStep={stepCompleted} alternativeLabel>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label} </StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </Box>
+          </StepperRow>
+        </StepperContainer>
+
+        <Wrapper>
+          <OrderNoteButtonContainer>
+            <OrderTableContainer>
+              <OrderNoteTable>
+                <OrderNote>Your Order Summary </OrderNote>
+                <DelieveryAddressDate>
+                  <DelieveryDay>
+                    <LocalShipping />3 Days Delivery
+                  </DelieveryDay>
+                  <DeliveryAddress>
+                    Illustraion in Sketch or AI Includes: Sketch, PSD, PNG, SVG,
+                    AI Min: 1 illustraion
+                  </DeliveryAddress>
+                </DelieveryAddressDate>
+
+                <FlexContainer>
+                  <FlexItems heading>Date</FlexItems>
+                  <FlexItems>01 May 2023</FlexItems>
+                </FlexContainer>
+                <FlexContainer>
+                  <FlexItems heading>Order Id</FlexItems>
+                  <FlexItems>123456789</FlexItems>
+                </FlexContainer>
+                <FlexContainer>
+                  <FlexItems heading>Payment</FlexItems>
+                  <FlexItems>Cash on Delivery</FlexItems>
+                </FlexContainer>
+                <FlexContainer>
+                  <FlexItems heading>Delivery Charge </FlexItems>
+                  <FlexItems>
+                    <FormatPrice price="500" />
+                  </FlexItems>
+                </FlexContainer>
+                <FlexContainer>
+                  <FlexItems heading>Total Price </FlexItems>
+                  <FlexItems heading>
+                    <FormatPrice price="2500" />
+                  </FlexItems>
+                </FlexContainer>
+                <Link to="/">
+                  <AddToCart
+                    content="center"
+                    bgColor={({ theme }) => theme.colors.hover}
+                    width="100"
+                    radius="0.2"
+                    text="Order Details"
+                  />
+                </Link>
+              </OrderNoteTable>
+            </OrderTableContainer>
+            <ButtonContainer>
+              <CheckIconContainer>
+                <CheckIcon />
+              </CheckIconContainer>
+              <OrderConfirmMessage>
+                Your illustraion will go to you soon
+              </OrderConfirmMessage>
               <Link to="/">
                 <AddToCart
                   content="center"
-                  bgColor={({ theme }) => theme.colors.hover}
+                  bgColor={({ theme }) => theme.colors.primary}
                   width="100"
                   radius="0.2"
-                  text="Order Details"
+                  text="Continue Shopping"
                 />
               </Link>
-            </OrderNoteTable>
-          </OrderTableContainer>
-          <ButtonContainer>
-            <Link to="/">
-              <AddToCart
-                content="center"
-                bgColor={({ theme }) => theme.colors.primary}
-                width="100"
-                radius="0.2"
-                text="Continue Shopping"
-              />
-            </Link>
-            <Link to="/">
-              <AddToCart
-                content="center"
-                bgColor={({ theme }) => theme.colors.star}
-                width="100"
-                radius="0.2"
-                text="Order Tracking"
-              />
-            </Link>
+              <Link to="/">
+                <AddToCart
+                  content="center"
+                  bgColor={({ theme }) => theme.colors.star}
+                  width="100"
+                  radius="0.2"
+                  text="Order Tracking"
+                />
+              </Link>
 
-            <Link to="/">
-              <AddToCart
-                content="center"
-                bgColor={({ theme }) => theme.colors.gray}
-                width="100"
-                radius="0.2"
-                text="Contact Us"
-              />
-            </Link>
-          </ButtonContainer>
-        </OrderNoteButtonContainer>
-      </Wrapper>
+              <Link to="/">
+                <AddToCart
+                  content="center"
+                  bgColor={({ theme }) => theme.colors.gray}
+                  width="100"
+                  radius="0.2"
+                  text="Contact Us"
+                />
+              </Link>
+            </ButtonContainer>
+          </OrderNoteButtonContainer>
+        </Wrapper>
+      </Row>
     </Container>
   );
 };
--- src/components/stepper/stepper.style.jsx
+++ src/components/stepper/stepper.style.jsx
@@ -1,11 +1,37 @@
 import styled, { css } from "styled-components";
 import { mobile } from "../../responsive";
+import { Check } from "@mui/icons-material";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-bottom: 4rem;
+`;
+
+export const Row = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  
+`;
+
+export const StepperContainer = styled.div`
+  
+  margin: 0 auto;
+  border: 1px solid #ececec;
+  &:nth-child(1) {
+  }
+`;
+
+export const StepperRow = styled.div`
+  width: 40vw;
+  margin: 0 auto;
+  padding: 1rem;
+  
+`;
 
 export const Wrapper = styled.div`
   padding: 2rem;
-  margin: 2rem auto;
+  border: 1px solid #ececec;
+ 
+  
 `;
 
 export const OrderNoteButtonContainer = styled.div`
@@ -15,19 +41,39 @@ export const OrderNoteButtonContainer = styled.div`
   width: 70vw;
   margin: 0 auto;
 
+
   ${mobile({ flexDirection: "column", gap: "2rem", width: "100%" })}
 `;
 
+export const DelieveryAddressDate = styled.div`
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 0.7rem;
+`;
+
+export const DelieveryDay = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const DeliveryAddress = styled.p``;
+
 export const OrderTableContainer = styled.div`
+
+  border-right: 2px solid teal;
+  background: #fafafc;
   /**Media Query */
   ${mobile({ width: "100%" })}
 `;
 
 export const OrderNoteTable = styled.div`
-  border: 2px dashed ${({ theme }) => theme.colors.primary};
+  // border: 2px dashed ${({ theme }) => theme.colors.primary};
   padding: 1rem;
-  border-radius: 0.3rem;
-  width: 350px;
+  // border-radius: 0.3rem;
+  width: 450px;
 
   /**Media Query */
   ${mobile({ width: "100%" })}
@@ -39,10 +85,8 @@ export const OrderNote = styled.p`
   letter-spacing: 2px;
   line-height: 5px;
   margin: 1rem 0;
-  background-color: teal;
+
   padding: 1rem 0.5rem;
-  color: white;
-  border-radius: 0.2rem;
   text-align: center;
 `;
 
@@ -52,7 +96,7 @@ export const FlexContainer = styled.div`
   justify-content: space-between;
   border-bottom: 1px dashed ${({ theme }) => theme.colors.gray};
 
-  &:last-child {
+  &:nth-last-child(2) {
     border-bottom: 0;
   }
 `;
@@ -72,8 +116,29 @@ export const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  margin: 0 1rem;
+  width: 350px;
+  height: 480px;
+  text-align: center;
+  padding: 50px;
+  background-color: white;
+  
 
   /**Media Query */
   ${mobile({ width: "100%" })}
 `;
+
+export const CheckIconContainer = styled.p`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background: green;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  margin:0 auto;
+`;
+
+export const CheckIcon = styled(Check)``;
+
+export const OrderConfirmMessage = styled.p``;
