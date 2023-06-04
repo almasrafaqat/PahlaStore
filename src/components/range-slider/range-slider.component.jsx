bfe06a536179b62c4acdc89e182a50f5381d1ecb
almasrafaqat <almasrafaqat6@gmail.com>
2023-06-04 20:20:10 +0300

    Slider Change HTML CSS

--- src/components/range-slider/range-slider.component.jsx
+++ src/components/range-slider/range-slider.component.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { FilterPriceInput, Main, PriceRange, ProgressBar, Selctor, SelctorValue, SelectBtn, Selector, TextValue } from './range-slider.style'
+import { Field, FilterPriceInput, InputSlider, Main, MaxValue, MinValue, PriceRange, ProgressBar, Range, RangeValue, Selctor, SelctorValue, SelectBtn, Selector, SliderValue, TextValue, ValueSpan } from './range-slider.style'
 
 const RangeSlider = ({ min, max, value }) => {
   const [rangevalue, setRangeValue] = useState(1500);
@@ -13,17 +13,28 @@ const RangeSlider = ({ min, max, value }) => {
 
   }
   return (
-    <Main>
-      {/* <FilterPriceInput defaultValue={max} type="range" min={min} max={max} /> */}
-      <PriceRange type="range" min="0" max="1500" value={rangevalue} onChange={PriceRangeHandler} />
-      <Selector left={rangevalue / 1500 * 100}>
-        <SelctorValue>
-          <TextValue>{rangevalue}</TextValue>
-        </SelctorValue>
+    // <Main>
+    //   {/* <FilterPriceInput defaultValue={max} type="range" min={min} max={max} /> */}
+    //   <PriceRange type="range" min="0" max="1500" value={rangevalue} onChange={PriceRangeHandler} />
+    //   <Selector left={rangevalue / 1500 * 100}>
+    //     <SelctorValue>
+    //       <TextValue>{rangevalue}</TextValue>
+    //     </SelctorValue>
 
-      </Selector>
-      <ProgressBar width={rangevalue / 1500 * 100} />
-    </Main>
+    //   </Selector>
+    //   <ProgressBar width={rangevalue / 1500 * 100} />
+    // </Main>
+
+    <Range>
+      <SliderValue>
+        <ValueSpan left={rangevalue /2} >{rangevalue}</ValueSpan>
+      </SliderValue>
+      <Field>
+        <MinValue>0</MinValue>
+          <InputSlider type="range" min="0" max="200" value={rangevalue} onChange={PriceRangeHandler} />
+        <MaxValue>1500</MaxValue>
+      </Field>
+    </Range>
   )
 }
 
--- src/components/range-slider/range-slider.style.jsx
+++ src/components/range-slider/range-slider.style.jsx
@@ -130,20 +130,148 @@ export const Main = styled.div`
   width: 100%;
   height: 100px;
   position: relative;
- 
 `;
 
 export const PriceRange = styled.input`
- 
- -webkit-appearance: none;
- background-color: lightgray;
+  -webkit-appearance: none;
+  background-color: lightgray;
   width: 100%;
   height: 4px;
   outline: none;
   border-radius: 8px;
   position: absolute;
- 
 
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background-color: white;
+    border: 1px solid teal;
+    cursor: pointer;
+    z-index: 3;
+    position: relative;
+  }
+`;
+
+export const Selector = styled.div`
+  width: 40px;
+  height: 104px;
+  position: absolute;
+  bottom: -20px;
+  left: ${(props) => props.left}%;
+  /* transform: translateX(-30px); */
+  z-index: 2;
+`;
+
+export const SelctorValue = styled.div`
+  width: 40px;
+  height: 45px;
+  position: absolute;
+  top: 2px;
+  border-radius: 4px;
+  text-align: center;
+  line-height: 45px;
+  font-size: 16px;
+  font-weight: 300;
+  background-color: orange;
+  z-index: 5;
+  color: white;
+
+  &::before {
+    content: "";
+    border-bottom: 17px solid orange;
+    border-left: 20px solid white;
+    border-right: 20px solid white;
+    position: absolute;
+    top: 0px;
+    left: 0;
+    z-index: 4;
+  }
+`;
+
+export const TextValue = styled.div`
+  margin-top: 5px;
+`;
+
+export const ProgressBar = styled.div`
+  width: ${(props) => props.width}%;
+  height: 4px;
+  border-radius: 8px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: teal;
+`;
+
+export const Range = styled.div`
+  height: 80px;
+  width: 100%;
+  background: #f0f0f0;
+  border-radius: 8px;
+  padding: 0 65px 0 45px;
+`;
+
+export const SliderValue = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const ValueSpan = styled.span`
+  position: absolute;
+  height: 45px;
+  width: 45px;
+  color: #fff;
+  font-weight: 500;
+  top: -30px;
+  left: ${(props) => props.left}%;
+  transform: translateX(-50%);
+  line-height: 55px;
+  z-index: 2;
+
+  &:after{
+    position: absolute;
+    content: '';
+    height: 45px;
+    width: 45px;
+    background: #664aff;
+    left: ${(props) => props.left}%;
+    transform: translateX(-50%) rotate(45deg);
+    border: 3px solid teal;
+    z-index: -1;
+    border-top-left-radius: 50%;
+    border-top-right-radius: 50%;
+    border-bottom-left-radius: 50%;
+  }
+
+`;
+
+export const Field = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
+
+export const MinValue = styled.div`
+  position: absolute;
+  font-size: 18px;
+  font-weight: bold;
+  color: #664aff;
+  left: -22px;
+`;
+
+export const InputSlider = styled.input`
+  -webkit-appearance: none;
+  height: 3px;
+  background: #ddd;
+  border-radius: 5px;
+  outline: none;
+  border: none;
+
+  
   ::-webkit-slider-thumb {
     -webkit-appearance: none;
     width: 24px;
@@ -158,59 +286,10 @@ export const PriceRange = styled.input`
 
 `;
 
-export const Selector = styled.div`
-  width: 40px;
-  height: 104px;
-  position: absolute;
-  bottom: -20px;
-  left: ${(props) => props.left}%;
-  /* transform: translateX(-30px); */
-  z-index: 2;
- 
-  
-`;
-
-export const SelctorValue = styled.div`
- width: 40px;
- height: 45px;
- position: absolute;
- top:2px;
- border-radius: 4px;
- text-align: center;
- line-height: 45px;
- font-size: 16px;
- font-weight: 300;
- background-color: orange;
- z-index: 5;
-color: white;
- 
-&::before{
-  content: '';
-  border-bottom: 17px solid orange;
-  border-left: 20px solid white;
-  border-right: 20px solid white;
-  position: absolute;
-  top: 0px;
-  left: 0;
-  z-index: 4;
-
-}
-
-`;
-
-export const TextValue = styled.div`
-  
-  margin-top: 5px;
-`;
-
-
-
-export const ProgressBar = styled.div`
-  width: ${(props) => props.width}%;
-  height: 4px;
-  border-radius: 8px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  background-color: teal;
+export const MaxValue = styled.div`
+position: absolute;
+font-size: 18px;
+font-weight: bold;
+color: #664aff;
+right: -43px;
 `;
